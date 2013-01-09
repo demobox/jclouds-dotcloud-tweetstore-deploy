50d2f0beb95e10708fe3c4ce9f74712fd855be63
Andrew Phillips <demobox1@yahoo.co.uk>
2013-01-09 18:46:22 -0500

    Using internal hostname for enqueue-store-tweets task

--- jobs/tweetstore/enqueue-store-tweet-tasks.sh
+++ jobs/tweetstore/enqueue-store-tweet-tasks.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -w "|%{url_effective} %{http_code} %{http_connect} %{time_total}" -H "X-Dotcloud-Cron: true" $DOTCLOUD_WWW_HTTP_URL/stores/do | grep -Po '\|.*'
+curl -w "|%{url_effective} %{http_code} %{http_connect} %{time_total}" -H "X-Dotcloud-Cron: true" http://$HOSTNAME:$PORT_HTTP/stores/do | grep -Po '\|.*'
