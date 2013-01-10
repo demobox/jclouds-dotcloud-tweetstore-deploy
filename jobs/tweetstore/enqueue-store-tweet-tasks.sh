d465fe3a36f92fd86cd5ec4d95221a82b9ec4b02
Andrew Phillips <demobox1@yahoo.co.uk>
2013-01-10 14:38:31 -0500

    Using -e instead of -a for file existence check in enqueue-stores task

--- jobs/tweetstore/enqueue-store-tweet-tasks.sh
+++ jobs/tweetstore/enqueue-store-tweet-tasks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 JOB_LOG_FILE=/home/dotcloud/code/jobs/enqueue-store-tweet-tasks.log
-if [ -a $JOB_LOG_FILE ] && [ `grep -c . $JOB_LOG_FILE` -gt 100 ]; then
+if [ -e $JOB_LOG_FILE ] && [ `grep -c . $JOB_LOG_FILE` -gt 100 ]; then
   rm $JOB_LOG_FILE
 fi
 curl -w "|%{url_effective} %{http_code} %{http_connect} %{time_total}" -H "X-Dotcloud-Cron: true" http://$DOTCLOUD_WWW_HTTP_HOST:$WWW_HTTP_PORT/stores/do | grep -Po '\|.*' >> $JOB_LOG_FILE
