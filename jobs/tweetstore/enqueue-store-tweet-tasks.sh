eaaf66c1e4c1ca8b1dc3672b829185c7c1160cda
Andrew Phillips <demobox1@yahoo.co.uk>
2013-01-10 13:03:40 -0500

    Using public IP and port for callbacks now to balance out admin operations

--- jobs/tweetstore/enqueue-store-tweet-tasks.sh
+++ jobs/tweetstore/enqueue-store-tweet-tasks.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-HOST_HTTP=`hostname`
-curl -w "|%{url_effective} %{http_code} %{http_connect} %{time_total}" -H "X-Dotcloud-Cron: true" http://$HOST_HTTP:$PORT_HTTP/stores/do | grep -Po '\|.*' >> ~/cron.log
+JOB_LOG_FILE=/home/dotcloud/code/jobs/enqueue-store-tweet-tasks.log
+if [ -a $JOB_LOG_FILE ] && [ `grep -c . $JOB_LOG_FILE` -gt 100 ]; then
+  rm $JOB_LOG_FILE
+fi
+curl -w "|%{url_effective} %{http_code} %{http_connect} %{time_total}" -H "X-Dotcloud-Cron: true" http://$DOTCLOUD_WWW_HTTP_HOST:$WWW_HTTP_PORT/stores/do | grep -Po '\|.*' >> $JOB_LOG_FILE
